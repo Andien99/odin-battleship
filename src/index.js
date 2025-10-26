e8e92bd7d88d65b4f2186637b9ec2b9b12ce95c2
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-10-27 09:56:54 +1100

    Feat: createPlayer.js will automatically assign a gameboard and determine if the opponent is a CPU or player.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-const { ship } = require("./createShip");
-const { gameboard } = require("./createGameboard");
-const board = new gameboard();
-board.createGameboard();
-const testShip1 = new ship(3, 0, false, "vertical");
-const testShip2 = new ship(2, 0, false, "horizontal");
-board.recieveAttack([1, "A"]);
-board.placeShip(testShip1, [7, "E"]);
+
